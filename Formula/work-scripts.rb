9978d2c7face602dba6d3be02d824c5011d7360c
Adam Petrovic <adam@petrovic.com.au>
2025-06-14 15:09:38 +1000

    add work-scripts formula

---
+++ Formula/work-scripts.rb
@@ -0,0 +1,11 @@
+class WorkScripts < Formula
+  desc "Collection of work utility scripts"
+  homepage "https://github.com/adampetrovic/work-scripts"
+  head "git@github.com:adampetrovic/work-scripts.git"
+
+  def install
+    bin.install Dir["scripts/*"]
+    # For Go scripts, you'd build them first:
+    # system "go", "build", "-o", "#{bin}/script-name", "script.go"
+  end
+end
