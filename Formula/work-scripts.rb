48d7814b195b7e64841678747c49a61649d1f959
Adam Petrovic <adam@petrovic.com.au>
2025-07-25 23:59:03 +1000

    work-scripts: add support for golang

--- Formula/work-scripts.rb
+++ Formula/work-scripts.rb
@@ -4,9 +4,17 @@ class WorkScripts < Formula
   head "https://github.com/adampetrovic/work-scripts.git", branch: "main"
 
   def install
+    # Install shell scripts
     Dir["*.sh"].each do |script|
       name_without_extension = File.basename(script, ".sh")
       bin.install script => name_without_extension
+    end
+
+    # Compile and install Go scripts
+    Dir["*.go"].each do |script|
+      name_without_extension = File.basename(script, ".go")
+      system "go", "build", "-o", name_without_extension, script
+      bin.install name_without_extension
     end
   end
 end
